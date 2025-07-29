9734ac8b711b1f2d1ede025b5d60035713efb3dc
imsparsh2001 <sparshjaipuriyar@gmail.com>
2025-07-29 15:52:54 +0530

    latest commit

--- log-rotation.sh
+++ log-rotation.sh
@@ -2,7 +2,7 @@
 
 # 3. Log Rotation Script
 # Compress logs older than 3 days in /var/log/,
-# move them to a backup directory, and delete all but the latest 3 backups.
+# move them to a backup directory, and deletes backups older than 30 days.
 
 
 log_dir="/var/log"
@@ -14,9 +14,7 @@ find "$log_dir" -type f -mtime +3 ! -name "*.zip" | while read -r file; do
     zip -m "${file}.zip" "$file"
 done
 
-# Move zip files to backup directory
 find "$log_dir" -type f -name "*.zip" -exec mv {} "$backup_dir" \;
 
-# Delete zipped backups older than 30 days in backup directory
 find "$backup_dir" -type f -name "*.zip" -mtime +30 -exec rm -f {} \;
 
